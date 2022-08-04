e87dd3fc2b6dc937e9fd629db831417e50f8124e
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-08-04 17:08:32 +0500

    first commit

---
+++ lib/github_oauth_credentials.dart
@@ -0,0 +1,6 @@
+// TODO(CodelabUser): Create an OAuth App
+const githubClientId = '4d0f0fbaa3e41f85caf1';
+const githubClientSecret = 'Y19175808719548cc88025537a5eb8dcc1fa3f795';
+
+// OAuth scopes for repository and user information
+const githubScopes = ['repo', 'read:org'];
